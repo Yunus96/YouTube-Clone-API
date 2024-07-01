b0467c55547f9037b376dc7d88bd7ca0d3195634
yunus102 <itsyunus102@gmail.com>
2024-07-01 14:48:30 +0530

    set up completed

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -5,5 +5,25 @@ import fs from 'fs'
     cloudinary.config({ 
         CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
         CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
-        CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET// Click 'View Credentials' below to copy your API secret
+        CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET
     });
+
+    const uploadOnCloudinary = async (localFilePath) => {
+        try {
+            if (!localFilePath) {
+                console.log("couldn't find local file path")
+            } else {
+                //upload the file on cloudinary
+                const response = await cloudinary.uploader.upload(localFilePath, {
+                    resource_type: 'auto'
+                })
+                return response;
+            }
+
+        } catch (error) {
+           fs.unlinkSync(localFilePath) 
+           return null;
+        }
+    }
+    
+    export {uploadOnCloudinary}
