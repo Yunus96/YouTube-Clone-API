a615b5dcfb06acf37a147948de068c464f0b92c1
yunus102 <itsyunus102@gmail.com>
2024-07-07 17:50:32 +0530

    adding logic

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,23 @@
 import {asyncHandler} from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js"
+import { User } from "../models/user.model.js"
+
 
 const registerUser = asyncHandler(async (req, res) => {
     const { fullName, email, username, password } = req.body
-    console.log(email);
+
+    if(
+        [fullName, email, username, password].some((field) => field?.trim()==="")
+    ){
+        throw new ApiError(400, "all fields are required")
+    }
+
+    const existedUser = User.findOne({
+        $or: [{ username }, { email }]
+    })
+    if (existedUser){
+        throw new ApiError(409, "User with email or username already exist")
+    }
 })
 
 
