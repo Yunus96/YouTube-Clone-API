42af22a92d5d0149f17f7f74e39c0d4b473a3289
yunus102 <itsyunus102@gmail.com>
2024-07-07 14:24:24 +0530

    minor changes in controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,9 +3,6 @@ import {asyncHandler} from "../utils/asyncHandler.js";
 const registerUser = asyncHandler(async (req, res) => {
     const { fullName, email, username, password } = req.body
     console.log(email);
-    res.status(200).json({
-        messgae: "ok"
-    })
 })
 
 
