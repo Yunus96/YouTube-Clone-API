d4ded144f8735f6eb2ffed6a9192d0decdbc9db0
yunus102 <itsyunus102@gmail.com>
2024-07-08 15:06:08 +0530

    added registering logic

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,7 +2,8 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js"
 import { User } from "../models/user.model.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js"
-
+import { ApiResponse } from "../utils/ApiResponse.js"
+ 
 const registerUser = asyncHandler(async (req, res) => {
     const { fullName, email, username, password } = req.body
 
@@ -33,7 +34,7 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new ApiError(400, "Avatar image is requried")
     }
 
-    User.create({
+    const user = await User.create({
         fullName,
         avatar: avatar.url,
         coverImage: coverImage?.url || "",
@@ -41,6 +42,21 @@ const registerUser = asyncHandler(async (req, res) => {
         password,
         username: username.toLowerCase()
     })
+
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    if (!createdUser) {
+        throw new ApiError(
+            500,
+            "Something went error while registering a user"
+            )
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200, createdUser, "User registered successfully")
+    )
 })
 
 
