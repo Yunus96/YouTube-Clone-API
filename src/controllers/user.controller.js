cea712c01476d31e8c598c7609f1e6226fc7efa3
yunus102 <itsyunus102@gmail.com>
2024-07-08 14:21:06 +0530

    adding more logic to controllers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,7 @@
-import {asyncHandler} from "../utils/asyncHandler.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js"
 import { User } from "../models/user.model.js"
-
+import { uploadOnCloudinary } from "../utils/cloudinary.js"
 
 const registerUser = asyncHandler(async (req, res) => {
     const { fullName, email, username, password } = req.body
@@ -17,6 +17,13 @@ const registerUser = asyncHandler(async (req, res) => {
     })
     if (existedUser){
         throw new ApiError(409, "User with email or username already exist")
+    }
+
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0].path;
+
+    if(!avatarLocalPath) {
+        throw new ApiError(400, "Avatar image is required")
     }
 })
 
