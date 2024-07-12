8f02bea6f12b7baa8dc14979e192a1115207d9e4
yunus102 <itsyunus102@gmail.com>
2024-07-12 18:05:06 +0530

    adding middleware

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -9,7 +9,11 @@ const generateAccessAndRefereshTokens = async (userId)=>{
          const user = await User.findById(userId);
          const accessToken = user.generateAccessToken();
          const refreshToken = user.generateRefreshToken();
-            
+         
+         user.refreshToken = refreshToken
+         await user.save({ validateBeforeSave : false })
+
+         return { accessToken, refreshToken }
     } catch (error) {
         throw new ApiError(500, "Something went wrong while generating tokens")
     }
@@ -105,6 +109,31 @@ const loginUser = asyncHandler(async (req, res)=>{
         throw new ApiError(401, "Username or password is incorrect")
     }
 
+    const {accessToken, refreshToken} = await generateAccessAndRefereshTokens(user._id)
+
+    const loggedInUser = await User.findById(user._id).
+    select("-password -refreshToken")
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res.status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+        new ApiResponse(
+            200,
+            {
+                user: loggedInUser, accessToken, refreshToken
+            },
+            "User logged in successfully"
+        )
+    )
+})
+
+const logoutUser = asyncHandler(async (req, res)=>{
 
 })
 
