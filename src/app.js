71a19decbbc36995930f8ec11bca46e24d96fbdb
Yunus96 <mohammadyunus102@gmail.com>
2025-07-30 09:15:10 +0530

    removed swagger folder and added videorouter

--- src/app.js
+++ src/app.js
@@ -24,51 +24,33 @@ app.use(cookieParser())
 
 // routes import
 import userRouter from "./routes/user.routes.js"
-/*
 import healthcheckRouter from "./routes/healthcheck.routes.js"
+import videoRouter from "./routes/video.routes.js"
+
+/*
 import tweetRouter from "./routes/tweet.routes.js"
 import subscriptionRouter from "./routes/subscription.routes.js"
-import videoRouter from "./routes/video.routes.js"
 import commentRouter from "./routes/comment.routes.js"
 import likeRouter from "./routes/like.routes.js"
 import playlistRouter from "./routes/playlist.routes.js"
 import dashboardRouter from "./routes/dashboard.routes.js"
 */
 
-const options = {
-    definition: {
-        openapi: "3.0.0",
-        info: {
-            title: "YouTube Clone API",
-            version: "1.0.0",
-            description: "YouTube Clone API",
-        },
-        servers: [
-            {
-                url: "https://youtube-clone-api-zqkd.onrender.com",
-            },
-        ],
-    },
-    apis: ["./src/routes/*.routes.js"],
-};
 
 //routes declaration
 app.use("/api/v1/users", userRouter)
-
-
 app.use("/api/v1/healthcheck", healthcheckRouter)
+app.use("/api/v1/videos", videoRouter)
 /*
 app.use("/api/v1/tweets", tweetRouter)
 app.use("/api/v1/subscriptions", subscriptionRouter)
-app.use("/api/v1/videos", videoRouter)
+
 app.use("/api/v1/comments", commentRouter)
 app.use("/api/v1/likes", likeRouter)
 app.use("/api/v1/playlist", playlistRouter)
 app.use("/api/v1/dashboard", dashboardRouter)
 */
 
-const specs = swaggerDoc(options);
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 // http://localhost:8000/api/v1/users/register
 
 export { app }
